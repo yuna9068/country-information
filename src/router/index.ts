adfb17735172a6ba1fd27039e6f3257c0039f450
Yuna <yuna9068@gmail.com>
2023-10-04 11:10:51 +0800

    feat: 修改網頁標題和 Favicon

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { storeToRefs } from 'pinia';
 import HomeView from '@/views/HomeView.vue';
+import useCountryStore from '@/stores/country';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,12 +19,32 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('@/views/AboutView.vue'),
     },
   ],
+});
+
+router.beforeEach((to) => {
+  const defaultTitle = 'Where in the world?';
+  const countryStore = useCountryStore();
+  const { getSelectedCountry } = storeToRefs(countryStore);
+  const countryName = getSelectedCountry.value.name;
+  let title = defaultTitle;
+
+  switch (to.name) {
+    case 'detail':
+      title = `${countryName} | ${defaultTitle}`;
+      break;
+
+    case 'about':
+      title = `About | ${defaultTitle}`;
+      break;
+
+    default:
+      break;
+  }
+
+  document.title = title;
 });
 
 export default router;
